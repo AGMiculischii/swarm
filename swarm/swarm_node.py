41a38dd86275bccd1fd90891b6f9b658525cbffc
Vladimir Petrigo <taenaru@gmail.com>
2015-05-29 22:58:09 +0300

    Do some cleaning on code

--- swarm/swarm_node.py
+++ swarm/swarm_node.py
@@ -3,22 +3,28 @@ import serial
 import serial.tools.list_ports
 import colorama
 
+
 def get_ports():
     if sys.platform.startswith('win32'):
-        com_ports = [(name, desc.encode('utf-16-be').decode('cp1251'), v_p) for name, desc, v_p in serial.tools.list_ports.comports()]
-
-    s_cp = sorted(com_ports, key = lambda tup: tup[0])
+        com_ports = [(name, desc.encode('utf-16-be').decode('cp1251'), v_p)
+                     for name, desc, v_p
+                     in serial.tools.list_ports.comports()]
+    else:
+        raise NotImplementedError('Add functionality for other than win32 systems')
+    s_cp = sorted(com_ports, key=lambda tup: tup[0])
     for num, port in enumerate(s_cp):
         print('{0} - PORT: {1[0]}, DESCRIPTION: {1[1]}'.format(num, port))
 
     return s_cp
 
+
 def select_port(cp_list):
     num = int(input('Which port do you want to select?: '))
     return cp_list[num][0]
 
+
 class SerCom:
-    '''
+    """
     This is the wrapper class for pyserial API
 
     @__init__ : Accepts a port name.
@@ -30,13 +36,14 @@ class SerCom:
 
     @send_command(command): Pass the string you want to be sent to the port.
     At the end of the command adds _RETEND bytestring
-    '''
+    """
+
     _RETEND = b'\r\n'
 
     def __init__(self, port):
         self.port = port
 
-    def open_port(self, baud = 115200):
+    def open_port(self, baud=115200):
         try:
             self.ser = serial.Serial(self.port, baud)
             print("Port %s is opened now at baud %s" % (self.port, baud))
@@ -55,26 +62,29 @@ class SerCom:
         b_com = command.encode('utf-8') + self._RETEND
         self.ser.write(b_com)
 
+
 class SwarmNode(SerCom):
     CUR_FILE = r'D:\PVE\Utilities\cur.txt'
     __rato_buf = []
     __rrn_buf = []
     __buffer = []
     __COMMANDS = {
-                    'RATO_WO_TO' : 'RATO 0 ', 
-                    'RATO_W_TO' : 'RATO 1 ', 
-                    'GET_SET' : 'GSET',
-                    'RES_SET' : 'RSET',
-                    'SAVE_SET' : 'SSET',
-                    'GET_ID' : 'GNID'
-                  }
+        'RATO_WO_TO': 'RATO 0 ',
+        'RATO_W_TO': 'RATO 1 ',
+        'GET_SET': 'GSET',
+        'RES_SET': 'RSET',
+        'SAVE_SET': 'SSET',
+        'GET_ID': 'GNID'
+    }
+
     __OUT_LEN = {
-            'RATO' : 3,
-            'RRN' : 6
-        }
+        'RATO': 3,
+        'RRN': 6
+    }
+
     __NOTIF = ['RRN', 'NIN', 'AIR', 'SDAT', 'DNO']
 
-    def __init__(self, port, disp_dist = False, disp_rrn = False):
+    def __init__(self, port, disp_dist=False, disp_rrn=False):
         SerCom.__init__(self, port)
         self.disp_dist = disp_dist
         self.dist_rrn = disp_rrn
@@ -136,13 +146,13 @@ class SwarmNode(SerCom):
         t_buf = self.__split_buf_msg(self.__rato_buf, self.__OUT_LEN['RATO'])
         sz = len(t_buf)
         if len(t_buf) != 0:
-            for err, d, rssi in t_buf:
+            for err, d, rssi in iter(t_buf):
                 if not err[-1] == 0:
                     sz -= 1
                     continue
                 dist += int(d) / 100
-        #num_val = len(self.__rato_buf)
-        #if not num_val == 0:
+        # num_val = len(self.__rato_buf)
+        # if not num_val == 0:
         #    for msg in self.__rato_buf:
         #        msg = msg.split(',')
         #        if not len(msg) == 3:
@@ -153,21 +163,21 @@ class SwarmNode(SerCom):
         #            continue
         dist = dist / sz if not sz == 0 else 0
 
-        out_str = colorama.Style.BRIGHT + colorama.Fore.CYAN + "Current distance: {:>5.2f} m".format(dist) + colorama.Style.RESET_ALL
+        out_str = colorama.Style.BRIGHT + colorama.Fore.CYAN + \
+            "Current distance: {:>5.2f} m".format(dist) +\
+            colorama.Style.RESET_ALL
 
-        #print( out_str , end = '\r')
-        print(out_str, file = open(self.CUR_FILE, 'a'))
+        # print( out_str , end = '\r')
+        print(out_str, file=open(self.CUR_FILE, 'a'))
         return out_str
 
     def __display_rrn(self):
         dist = 0
         t_buf = self.__split_buf_msg(self.__rato_buf, self.__OUT_LEN['RRN'])
         sz = len(t_buf)
-        if not num_val == 0:
+        if not sz == 0:
             for msg in self.__rrn_buf:
-                msg = msg.split(',')
-            
-
+                `msg = msg.split(',')
 
         pass
 
@@ -187,7 +197,7 @@ class SwarmNode(SerCom):
 
         if self.disp_dist:
             o_str = self.__display_rato()
-
+            print(o_str, end='\r')
 
         self.__rato_buf.clear()
 
